a12dc42f8dbec77cd227dd6bebb3c814d3ca884c
Gizmotronn <liam@skinetics.tech>
2025-05-15 09:50:02 +0800

    Initial Commit

---
+++ Trackme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trackme
+//
+//  Created by Liam Arbuckle on 15/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trackme/TrackmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackmeApp.swift
+//  Trackme
+//
+//  Created by Liam Arbuckle on 15/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
